e0c8a254bbbbae9151f1cae364e578f7ed83f6bf
scalaragg <dlaeve321@gmail.com>
2026-09-07 13:58:58 +0300

    0 0 02

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-"""Основной файл приложения Task manadger
+"""## Версия 0.0.2
+-[x] реализовать место хранения задач
+-[x] сделать функцию - показать заметки
+- [x] сделать функци - создать заметки
 """
+
+collection = [] #list
+
 is_start = True #flag
 while is_start:
-    print('hello world')
-    is_start = False
-    print('Finish')
+    print("1 - показать задачи | 2 - добавить заметку")
+    choice_user = input('введите ваш выбор (1 или 2)')
+    match int(choice_user):
+        case 1:
+            print(collection)
+        case 2:
+            collection.append('task')
+            print(collection)
+        case _:
+            print('такого пункта нет!')
